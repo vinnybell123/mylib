a2bc72d139fec41450db1fe4ecc1a07cc91b0d12
vinnybell123 <136488782+vinnybell123@users.noreply.github.com>
2023-07-17 23:02:02 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,3 +6,7 @@ def build()
 {
   sh 'mvn package'
 }
+def deploy(ip,appname)
+{
+  deploy adapters: [tomcat9(credentialsId: '2a9c8782-4838-42ff-bbdd-4d8facd689ca', path: '', url: "${ip}")], contextPath: "${appname}", war: '**/*.war'
+}
